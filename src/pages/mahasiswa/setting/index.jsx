55ab64f04210bb22f53f484e9ec5ee3b850f729b
Azisprtm_ <azisprtm2@gmail.com>
2023-12-23 15:55:03 +0700

    clear console.log/error

--- src/pages/mahasiswa/setting/index.jsx
+++ src/pages/mahasiswa/setting/index.jsx
@@ -6,17 +6,25 @@ import ChangePasswordModule from "../../../modules/setting/change-password";
 import AkunModule from "../../../modules/setting/akun";
 
 export default function Settings() {
-	const { user } = useUser({ redirectTo: "/login" });
-	const { prefix, menu, active, setActive } = useMenu();
+  const { user } = useUser({ redirectTo: "/login" });
+  const { prefix, menu, active, setActive } = useMenu();
 
-	if ([user, menu].some((item) => item == null)) return <p>Loading...</p>;
-	return (
-		<Layout>
-			<PageHeader title={menu.label} icon={menu.icon} items={menu.submenus} active={active.url} handler={setActive} />
-			<div className="my-8">
-				{active.url === "#change-password" && <ChangePasswordModule baseURL={prefix + menu.url} />}
-				{active.url === "#akun" && <AkunModule baseURL={prefix + menu.url} />}
-			</div>
-		</Layout>
-	);
+  if ([user, menu].some((item) => item == null)) return <p>Loading...</p>;
+  return (
+    <Layout>
+      <PageHeader
+        title={menu.label}
+        icon={menu.icon}
+        items={menu.submenus}
+        active={active.url}
+        handler={setActive}
+      />
+      <div className="my-8">
+        {active.url === "#change-password" && (
+          <ChangePasswordModule baseURL={prefix + menu.url} />
+        )}
+        {active.url === "#akun" && <AkunModule baseURL={prefix + menu.url} />}
+      </div>
+    </Layout>
+  );
 }
