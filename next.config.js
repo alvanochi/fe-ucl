3f762cbcf83725c07ff33df6117fdf437e02d54b
Azisprtm_ <azisprtm2@gmail.com>
2024-01-03 15:34:27 +0000

    update next config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     APP_NAME: "TIAS",
     SESSION_SECRET_KEY: "B1zq1JKpYNcz8dgEeypiPrK3bZdeJZrQ",
-    API_ENDPOINT: "http://localhost:5000",
+    API_ENDPOINT: "http://103.158.196.33:8080/tias-be",
   },
 };
 
