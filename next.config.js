b50afee5fb43b65c15025e9eaa51dbd49adf70b1
Azisprtm_ <azisprtm2@gmail.com>
2024-02-01 21:47:11 +0700

    update next.config

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   env: {
     APP_NAME: "TIAS",
     SESSION_SECRET_KEY: "B1zq1JKpYNcz8dgEeypiPrK3bZdeJZrQ",
-    API_ENDPOINT: "http://localhost:5000",
+    API_ENDPOINT: "https://api-tias.ti.ft.uika-bogor.ac.id",
     API_ENDPOINT_ABSEN: "https://absen.ft.uika-bogor.ac.id/api"
   },
 };
