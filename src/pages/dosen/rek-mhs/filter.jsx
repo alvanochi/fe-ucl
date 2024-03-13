e9559b5697c534072dfcbdb5de38380f0bc45ee4
Azisprtm_ <azisprtm2@gmail.com>
2024-03-13 12:37:39 +0700

    update filter rek mhs

--- src/pages/dosen/rek-mhs/filter.jsx
+++ src/pages/dosen/rek-mhs/filter.jsx
@@ -4,59 +4,69 @@ import Modal from "../../../components/Modal";
 import Form from "../../../components/Form";
 import useModal from "../../../hooks/useModal";
 import useForm from "../../../hooks/useForm";
+import useMahasiswa from "../../../repo/mahasiswa";
 
 export default function Filter({ filter, handler }) {
-	const { form, inputHandler, setForm } = useForm(filter);
-	const { show, toggle, close } = useModal({
-		onClose: () => {
-			handler({});
-			setForm({});
-		},
-	});
+  const { form, inputHandler, setForm } = useForm(filter);
+  const { show, toggle, close } = useModal({
+    onClose: () => {
+      handler({});
+      setForm({});
+    },
+  });
 
-	const submitHandler = (event) => {
-		event.preventDefault();
-		handler(form);
-		toggle();
-	};
+  const submitHandler = (event) => {
+    event.preventDefault();
+    handler(form);
+    toggle();
+  };
 
-	return (
-		<>
-			<Button variant="primary" icon={<Icon icon="clarity:filter-line" width={20} height={20} />} onClick={toggle} pill>
-				Filter
-			</Button>
-			<Modal title="Filter Berdasarkan" show={show} handler={toggle}>
-				<Form onSubmit={submitHandler} className="grid grid-cols-2 gap-4">
-					<Form.Group>
-						<Form.Label>Nama Lengkap</Form.Label>
-						<Form.Input
-							type="text"
-							className="flex-1"
-							name="nama_lengkap"
-							value={form?.nama_lengkap ?? ""}
-							onChange={inputHandler}
-						/>
-					</Form.Group>
-					<Form.Group>
-						<Form.Label>NPM</Form.Label>
-						<Form.Input
-							type="text"
-							className="flex-1"
-							name="npm"
-							value={form?.npm ?? ""}
-							onChange={inputHandler}
-						/>
-					</Form.Group>
-					<Form.Group className="col-span-2 flex gap-2">
-						<Button type="button" variant="secondary" onClick={close}>
-							Kosongkan
-						</Button>
-						<Button variant="primary" className="grow">
-							Terapkan
-						</Button>
-					</Form.Group>
-				</Form>
-			</Modal>
-		</>
-	);
+  const { data: listMahasiswa } = useMahasiswa();
+
+  return (
+    <>
+      <Button
+        variant="primary"
+        icon={<Icon icon="clarity:filter-line" width={20} height={20} />}
+        onClick={toggle}
+        pill
+      >
+        Filter
+      </Button>
+      <Modal title="Filter Berdasarkan" show={show} handler={toggle}>
+        <Form onSubmit={submitHandler} className="grid grid-cols-2 gap-4">
+          <Form.Group className="col-span-2 flex gap-2">
+            <Form.Label className="min-w-[10rem]">
+              Nama <span className="text-danger-600">*</span>
+            </Form.Label>
+            <span>:</span>
+            <Form.Combobox
+              name="npm"
+              onChange={(selected) =>
+                inputHandler({
+                  target: {
+                    name: "npm",
+                    value: selected?.value,
+                  },
+                })
+              }
+              value={form?.npm ?? ""}
+              options={listMahasiswa?.map((mhs) => ({
+                label: mhs.nama_lengkap,
+                value: mhs.npm,
+              }))}
+            />
+          </Form.Group>
+          <Form.Group className="col-span-2 flex gap-2">
+            <Button type="button" variant="secondary" onClick={close}>
+              Kosongkan
+            </Button>
+            <Button variant="primary" className="grow">
+              Terapkan
+            </Button>
+          </Form.Group>
+        </Form>
+      </Modal>
+    </>
+  );
 }
