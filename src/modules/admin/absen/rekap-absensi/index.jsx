0f844b51026f5d3cc673939ec2fc0d09880e2c79
Azisprtm_ <azisprtm2@gmail.com>
2024-03-17 14:58:14 +0700

    update again

--- src/modules/admin/absen/rekap-absensi/index.jsx
+++ src/modules/admin/absen/rekap-absensi/index.jsx
@@ -8,35 +8,77 @@ import Form from "../../../../components/Form";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { toastAlert } from "../../../../lib/sweetalert";
 
 export default function RekapAbsensi({ baseURL }) {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true); // Tambahkan state untuk loading
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchTotalData = async () => {
       try {
         const response = await axios.get(
-          `${process.env.API_ENDPOINT}/absensi/list-dosen`
+          `${process.env.API_ENDPOINT_ABSEN}/pembelajaran/list-dosen-pertemuan`
         );
 
         const res = response.data.data;
 
         setData(res);
-        setLoading(false); // Set loading menjadi false setelah data berhasil diambil
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching pertemuan:", error);
-        setLoading(false); // Set loading menjadi false jika terjadi error
+        setLoading(false);
       }
     };
 
     fetchTotalData();
   }, []);
 
+  const GENERATE_URL = `${process.env.API_ENDPOINT}/absensi/persentase-dosen/excel`;
+
+  async function generate() {
+    try {
+      const response = await axios.get(GENERATE_URL, {
+        responseType: "blob",
+      });
+
+      const contentDisposition = response.headers["content-disposition"];
+      const filename = contentDisposition
+        .split("filename=")[1]
+        .replace(/"/g, "");
+
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", filename);
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+      window.URL.revokeObjectURL(url);
+
+      toastAlert("success", "Exported!");
+    } catch (error) {
+      if (error.name === "AxiosError") {
+        toastAlert("warning", error.response.data);
+        return;
+      }
+      toastAlert("error", error);
+    }
+  }
+
   return (
     <>
-      <div className="flex items-center justify-center gap-2 my-8"></div>
-
+      <div className="mb-2">
+        <div className="flex justify-end items-end">
+          <Button.Icon
+            variant="success"
+            icon={<Icon icon="ri:file-excel-2-line" width={40} height={40} />}
+            onClick={() => generate()}
+          />
+        </div>
+      </div>
       <table
         className="w-full border-collapse rounded-2xl overflow-hidden shadow table-auto"
         cellPadding={10}
@@ -93,27 +135,26 @@ export default function RekapAbsensi({ baseURL }) {
                   {index + 1}
                 </td>
                 <td className="text-sm border-2 border-white bg-gray-50 ">
-                  {row.nidn}
+                  {row.nik}
                 </td>
                 <td className="text-sm border-2 border-white bg-gray-50 ">
                   <Link
-                    href={`${baseURL}/rekap-absensi/${row.nip}`}
+                    href={`${baseURL}/rekap-absensi/${row.code_lecturer}`}
                     className="text-blue-500"
                   >
-                    {row.nama_lengkap}
+                    {row.name}
                   </Link>
                 </td>
                 <td className="text-sm border-2 border-white bg-gray-50 ">
-                  {row.gasal}
+                  {row.persentase_gasal}
                 </td>
                 <td className="text-sm border-2 border-white bg-gray-50 ">
-                  {row.genap}
+                  {row.persentase_genap}
                 </td>
               </tr>
             ))}
         </tbody>
       </table>
-
     </>
   );
 }
