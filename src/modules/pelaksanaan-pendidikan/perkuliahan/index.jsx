2f74a0d7e27d042aaa680876467e1327e0ce003a
Azisprtm_ <azisprtm2@gmail.com>
2024-04-23 20:17:17 +0700

    update list perkuliahan

--- src/modules/pelaksanaan-pendidikan/perkuliahan/index.jsx
+++ src/modules/pelaksanaan-pendidikan/perkuliahan/index.jsx
@@ -2,35 +2,57 @@ import { Icon } from "@iconify-icon/react";
 import Button from "../../../components/Button";
 import Pagination from "../../../components/Pagination";
 import useDatatable from "../../../hooks/useDatatable";
+import useNewDataTable from "../../../hooks/useNewDataTable";
 import useCRUD from "../../../hooks/useCRUD";
 import SortIcon from "../../../components/SortIcon";
 import Filter from "./filter";
 import Link from "next/link";
+import { useState } from "react";
+import Form from "../../../components/Form";
 
 export default function PerkuliahanModule({ baseURL }) {
-
   const DATA_URL = `${process.env.API_ENDPOINT}/help/skpi-perkuliahan`;
 
+  // const {
+  //   data,
+  //   loading,
+  //   page,
+  //   pageCount,
+  //   filter,
+  //   setPage,
+  //   setFilter,
+  //   canPrev,
+  //   canNext,
+  //   refresh,
+  //   sortBy,
+  //   getSortBy,
+  // } = useDatatable(DATA_URL);
+
+  const [searchValue, setSearchValue] = useState("");
+
   const {
-    data,
-    loading,
-    page,
-    pageCount,
-    filter,
-    setPage,
-    setFilter,
-    canPrev,
-    canNext,
-    refresh,
-    sortBy,
-    getSortBy,
-  } = useDatatable(DATA_URL);
+    dataNew,
+    loadingNew,
+    pageNew,
+    pageCountNew,
+    setPageNew,
+    sortByNew,
+    getSortByNew,
+  } = useNewDataTable(DATA_URL, {}, searchValue);
 
   return (
     <>
       <div className="flex items-center justify-center gap-2 mb-8">
-        <Filter filter={filter} handler={setFilter} />
-        
+        <div className="flex-shrink">
+          <Form.Input
+            type="text"
+            name="search"
+            placeholder="Search"
+            style={{ width: "400px" }}
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+          />
+        </div>
       </div>
       <table
         className="w-full border-collapse rounded-2xl overflow-hidden shadow table-auto"
@@ -39,11 +61,7 @@ export default function PerkuliahanModule({ baseURL }) {
         <thead>
           <tr>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
-                No
-              </div>
+              <div className="flex items-center gap-2 cursor-pointer">No</div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
               <div className="flex items-center gap-2 cursor-pointer">
@@ -51,44 +69,32 @@ export default function PerkuliahanModule({ baseURL }) {
               </div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
-                SKS
-              </div>
+              <div className="flex items-center gap-2 cursor-pointer">SKS</div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
+              <div className="flex items-center gap-2 cursor-pointer">
                 Kelas
               </div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
+              <div className="flex items-center gap-2 cursor-pointer">
                 Dosen
               </div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
+              <div className="flex items-center gap-2 cursor-pointer">
                 Hari Jam/Tgl
               </div>
             </th>
             <th className="text-sm border-2 border-white bg-gray-200">
-              <div
-                className="flex items-center gap-2 cursor-pointer"
-              >
+              <div className="flex items-center gap-2 cursor-pointer">
                 Ruangan
               </div>
             </th>
           </tr>
         </thead>
         <tbody>
-          {loading && (
+          {loadingNew && (
             <tr>
               <td
                 colSpan="6"
@@ -98,7 +104,7 @@ export default function PerkuliahanModule({ baseURL }) {
               </td>
             </tr>
           )}
-          {!loading && data && data.length < 1 && (
+          {!loadingNew && dataNew && dataNew.length < 1 && (
             <tr>
               <td
                 colSpan="6"
@@ -108,10 +114,10 @@ export default function PerkuliahanModule({ baseURL }) {
               </td>
             </tr>
           )}
-          {!loading &&
-            data &&
-            data.map((row, index) => {
-              const startNumber = (page - 1) * 10 + 1;
+          {!loadingNew &&
+            dataNew &&
+            dataNew.map((row, index) => {
+              const startNumber = (pageNew - 1) * 10 + 1;
 
               const rowNumber = startNumber + index;
               return (
@@ -120,9 +126,12 @@ export default function PerkuliahanModule({ baseURL }) {
                     {rowNumber}
                   </td>
                   <td className="text-sm border-2 border-white bg-gray-50">
-                    <Link href={`${baseURL}/perkuliahan/list-perkuliahan/${row.course_code}-${row.class}`} className="text-blue-500">
+                    <Link
+                      href={`${baseURL}/perkuliahan/list-perkuliahan/${row.course_code}-${row.class}`}
+                      className="text-blue-500"
+                    >
                       {row.name}
-                  </Link>
+                    </Link>
                   </td>
                   <td className="text-sm border-2 border-white bg-gray-50">
                     {row.sks}
@@ -144,14 +153,60 @@ export default function PerkuliahanModule({ baseURL }) {
             })}
         </tbody>
       </table>
-      {/* <Pagination
-        current={page}
-        handler={setPage}
-        max={pageCount}
-        canPrev={canPrev()}
-        canNext={canNext()}
-        className="mt-8"
-      /> */}
+      <div className="flex mt-8">
+        <div className="flex gap-1 ml-auto">
+          <Button.Icon
+            type="button"
+            variant="outline-primary"
+            icon={
+              <Icon
+                icon="material-symbols:chevron-left"
+                width={20}
+                height={20}
+              />
+            }
+            onClick={() => setPageNew(pageNew - 1)}
+            disabled={pageNew <= 1}
+            pill
+          />
+          <Button
+            type="button"
+            variant="primary"
+            icon={
+              <Icon
+                icon="material-symbols:chevron-right"
+                width={20}
+                height={20}
+              />
+            }
+            iconPosition="right"
+            onClick={() => setPageNew(pageNew + 1)}
+            disabled={pageNew >= pageCountNew}
+            pill
+          >
+            Next Page
+          </Button>
+        </div>
+        <div className="ml-auto whitespace-nowrap flex items-center gap-2">
+          <p className="">Page</p>
+          <Form.Input
+            type="number"
+            min="1"
+            max={pageCountNew || 1}
+            className="w-20"
+            value={pageNew}
+            onChange={(event) =>
+              setPageNew(
+                Math.max(
+                  1,
+                  Math.min(event.target.valueAsNumber, pageCountNew || 1)
+                )
+              )
+            }
+          />
+          of {pageCountNew || 1}
+        </div>
+      </div>
     </>
   );
 }
