caa1fc86eb2aefcee602ff6fc016690f6fac7b9b
Azisprtm_ <azisprtm2@gmail.com>
2024-08-02 16:37:49 +0700

    solve detail rapat

--- src/pages/admin/jadwal-rapat/detail-list/absensi.jsx
+++ src/pages/admin/jadwal-rapat/detail-list/absensi.jsx
@@ -8,13 +8,15 @@ import useForm from "../../../../hooks/useForm";
 import { MySwal, loadingAlert, toastAlert } from "../../../../lib/sweetalert";
 import { use, useState } from "react";
 import useUsers from "../../../../repo/users";
+import useUser from "../../../../hooks/useUser";
 
 const AddAbsensiRapat = ({ data, onAddAbsensi }) => {
   const { show, toggle, close } = useModal();
   const [code, setCode] = useState("");
   const [name, setName] = useState("");
+  const { user } = useUser({ redirectTo: "/login" });
 
-  const { data: listUser } = useUsers();
+  const { data: listUser, isLoading: isUsersLoading } = useUsers([user]);
 
   const INITIAL_FORM = {
     id_meeting: data && data.id ? data.id : "",
@@ -77,89 +79,90 @@ const AddAbsensiRapat = ({ data, onAddAbsensi }) => {
     }
   }
 
-  return (
-    <>
-      <Button
-        variant="primary"
-        icon={<Icon icon="ic:baseline-plus" width={20} height={20} />}
-        onClick={toggle}
-        pill
-      >
-        Tambah Presensi
-      </Button>
-      <Modal title="Tambah Presensi" show={show} handler={toggle}>
-        <Form className="space-y-4" onSubmit={submitHandler}>
-          <Form.Input
-            type="hidden"
-            name="id_meeting"
-            onChange={inputHandler}
-            value={form.id_meeting}
-          />
-          <Form.Group className="flex items-baseline gap-3">
-            <Form.Label className="min-w-[10rem]">
-              Peserta <span className="text-danger-600">*</span>
-            </Form.Label>
-            <span>:</span>
-            <Form.Combobox
-              name="code"
-              onChange={handlePesertaChange}
-              value={code}
-              options={listUser?.map((user) => ({
-                label: `${user.nama_lengkap} - ${
-                  user.npm ? user.npm : user.nip
-                }`,
-                value: user.npm ? user.npm : user.nip,
-                name: user.nama_lengkap,
-              }))}
+  if ([user, isUsersLoading].some((item) => item == null))
+    return (
+      <>
+        <Button
+          variant="primary"
+          icon={<Icon icon="ic:baseline-plus" width={20} height={20} />}
+          onClick={toggle}
+          pill
+        >
+          Tambah Presensi
+        </Button>
+        <Modal title="Tambah Presensi" show={show} handler={toggle}>
+          <Form className="space-y-4" onSubmit={submitHandler}>
+            <Form.Input
+              type="hidden"
+              name="id_meeting"
+              onChange={inputHandler}
+              value={form.id_meeting}
             />
-          </Form.Group>
-          <Form.Group className="flex items-baseline gap-2">
-            <Form.Label className="min-w-[8rem] text-sm">
-              Status Absen <span className="text-danger-600">*</span>
-            </Form.Label>
-            <span>:</span>
-            <div className="flex gap-6">
-              <Form.Label>
-                <Form.Radio
-                  name="status_absen"
-                  value={1}
-                  onChange={inputHandler}
-                  checked={form.status_absen == 1}
-                />
-                Masuk
+            <Form.Group className="flex items-baseline gap-3">
+              <Form.Label className="min-w-[10rem]">
+                Peserta <span className="text-danger-600">*</span>
               </Form.Label>
-              <Form.Label>
-                <Form.Radio
-                  name="status_absen"
-                  value={2}
-                  onChange={inputHandler}
-                  checked={form.status_absen == 2}
-                />
-                Sakit / Izin
+              <span>:</span>
+              <Form.Combobox
+                name="code"
+                onChange={handlePesertaChange}
+                value={code}
+                options={listUser?.map((user) => ({
+                  label: `${user.nama_lengkap} - ${
+                    user.npm ? user.npm : user.nip
+                  }`,
+                  value: user.npm ? user.npm : user.nip,
+                  name: user.nama_lengkap,
+                }))}
+              />
+            </Form.Group>
+            <Form.Group className="flex items-baseline gap-2">
+              <Form.Label className="min-w-[8rem] text-sm">
+                Status Absen <span className="text-danger-600">*</span>
               </Form.Label>
-              <Form.Label>
-                <Form.Radio
-                  name="status_absen"
-                  value={0}
-                  onChange={inputHandler}
-                  checked={form.status_absen == 0}
-                />
-                Alfa
-              </Form.Label>
+              <span>:</span>
+              <div className="flex gap-6">
+                <Form.Label>
+                  <Form.Radio
+                    name="status_absen"
+                    value={1}
+                    onChange={inputHandler}
+                    checked={form.status_absen == 1}
+                  />
+                  Masuk
+                </Form.Label>
+                <Form.Label>
+                  <Form.Radio
+                    name="status_absen"
+                    value={2}
+                    onChange={inputHandler}
+                    checked={form.status_absen == 2}
+                  />
+                  Sakit / Izin
+                </Form.Label>
+                <Form.Label>
+                  <Form.Radio
+                    name="status_absen"
+                    value={0}
+                    onChange={inputHandler}
+                    checked={form.status_absen == 0}
+                  />
+                  Alfa
+                </Form.Label>
+              </div>
+            </Form.Group>
+            <div className="flex gap-4 mt-12">
+              <Button type="button" variant="secondary" onClick={close}>
+                Tutup
+              </Button>
+              <Button type="submit" variant="primary" className="w-full h-12">
+                Simpan
+              </Button>
             </div>
-          </Form.Group>
-          <div className="flex gap-4 mt-12">
-            <Button type="button" variant="secondary" onClick={close}>
-              Tutup
-            </Button>
-            <Button type="submit" variant="primary" className="w-full h-12">
-              Simpan
-            </Button>
-          </div>
-        </Form>
-      </Modal>
-    </>
-  );
+          </Form>
+        </Modal>
+      </>
+    );
 };
 
 export default AddAbsensiRapat;
