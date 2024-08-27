85dcdd56c9c2c4188fd983a22b41fd9c498d34e1
Azisprtm_ <azisprtm2@gmail.com>
2024-08-27 20:28:53 +0700

    update

--- src/pages/admin/jadwal-rapat/index.jsx
+++ src/pages/admin/jadwal-rapat/index.jsx
@@ -33,8 +33,6 @@ export default function JadwalRapat() {
     setFilter,
   } = useDatatableAbsensi(DATA_URL);
 
-  console.log(dataAbsensi);
-
   const { destroy } = useCRUD(DELETE_URL);
 
   if ([user, menu, loadingAbsensi].some((item) => item == null))
@@ -72,175 +70,181 @@ export default function JadwalRapat() {
             Sebaran
           </Button>
         </div>
-        <table
-          className="w-full border-collapse rounded-2xl overflow-hidden shadow"
-          cellPadding={10}
-        >
-          <thead>
-            <tr>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2 cursor-pointer">No</div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2 cursor-pointer">
-                  Pamflet
-                </div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2 cursor-pointer">
-                  Tipe Kegiatan
-                </div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2 cursor-pointer">
-                  Nama Kegiatan
-                </div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                Nararumber
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2">Ruangan</div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2">Tgl</div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                <div className="flex items-center gap-2">Waktu</div>
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                Status
-              </th>
-              <th className="text-sm border-2 border-white bg-gray-200">
-                Action
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {loadingAbsensi && (
+        <div className="overflow-x-auto">
+          <table
+            className="w-full border-collapse rounded-2xl overflow-hidden shadow"
+            cellPadding={10}
+          >
+            <thead>
               <tr>
-                <td
-                  colSpan="6"
-                  className="text-sm border-2 border-white bg-gray-50 text-center"
-                >
-                  Loading...
-                </td>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2 cursor-pointer">
+                    No
+                  </div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2 cursor-pointer">
+                    Pamflet
+                  </div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2 cursor-pointer">
+                    Tipe Kegiatan
+                  </div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2 cursor-pointer">
+                    Nama Kegiatan
+                  </div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  Nararumber
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2">Ruangan</div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2">Tgl</div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  <div className="flex items-center gap-2">Waktu</div>
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  Status
+                </th>
+                <th className="text-sm border-2 border-white bg-gray-200">
+                  Action
+                </th>
               </tr>
-            )}
-            {!loadingAbsensi && dataAbsensi && dataAbsensi.length < 1 && (
-              <tr>
-                <td
-                  colSpan="6"
-                  className="text-sm border-2 border-white bg-gray-50 text-center"
-                >
-                  Tidak ada data
-                </td>
-              </tr>
-            )}
-            {!loadingAbsensi &&
-              dataAbsensi &&
-              dataAbsensi.map((row, index) => {
-                const startNumber = (pageAbsensi - 1) * 10 + 1;
+            </thead>
+            <tbody>
+              {loadingAbsensi && (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="text-sm border-2 border-white bg-gray-50 text-center"
+                  >
+                    Loading...
+                  </td>
+                </tr>
+              )}
+              {!loadingAbsensi && dataAbsensi && dataAbsensi.length < 1 && (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="text-sm border-2 border-white bg-gray-50 text-center"
+                  >
+                    Tidak ada data
+                  </td>
+                </tr>
+              )}
+              {!loadingAbsensi &&
+                dataAbsensi &&
+                dataAbsensi.map((row, index) => {
+                  const startNumber = (pageAbsensi - 1) * 10 + 1;
 
-                const rowNumber = startNumber + index;
-                return (
-                  <tr key={`row-${index}`}>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {rowNumber}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      <Button.Icon
-                        onClick={() =>
-                          window.open(
-                            `${`https://absen.ft.uika-bogor.ac.id/storage/generatePamplet/${row.tipe_kegiatan}-${row.narsum}-(${row.tanggal}).png`}`,
-                            "_blank"
-                          )
-                        }
-                        variant="success"
-                        icon={
-                          <Icon
-                            icon="game-icons:target-poster"
-                            width={15}
-                            height={15}
-                          />
-                        }
-                      />
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.tipe_kegiatan}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.nm_kegiatan}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.narsum}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.ruangan}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.tanggal}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {`${row.waktu} - ${row.waktu_end}`}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50">
-                      {row.status_ruangan === 1
-                        ? "ONLINE"
-                        : row.status_ruangan === 0
-                        ? "OFFLINE"
-                        : "HYBRID"}
-                    </td>
-                    <td className="text-sm border-2 border-white bg-gray-50 max-w-[8rem] truncate mx-auto">
-                      <div className="flex items-stretch gap-1">
+                  const rowNumber = startNumber + index;
+                  return (
+                    <tr key={`row-${index}`}>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {rowNumber}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
                         <Button.Icon
-                          as="a"
-                          href={`${prefix + menu.url}/invite/${row.id}`}
-                          variant="primary"
-                          icon={
-                            <Icon
-                              icon="bi:person-fill-add"
-                              width={15}
-                              height={15}
-                            />
-                          }
-                        />
-                        <ShowQr
-                          data={{ token: row.token, kegiatan: row.nm_kegiatan }}
-                        />
-                        <Button.Icon
-                          as="a"
-                          href={`${prefix + menu.url}/detail-list/${row.id}`}
-                          variant="primary"
-                          icon={
-                            <Icon
-                              icon="bxs:message-square-edit"
-                              width={15}
-                              height={15}
-                            />
-                          }
-                        />
-                        <Button.Icon
-                          variant="danger"
-                          icon={
-                            <Icon
-                              icon="solar:trash-bin-2-bold-duotone"
-                              width={15}
-                              height={15}
-                            />
-                          }
                           onClick={() =>
-                            destroy(row.id).then(() => refreshAbsensi())
+                            window.open(
+                              `${`https://absen.ft.uika-bogor.ac.id/storage/generatePamplet/${row.tipe_kegiatan}-${row.narsum}-(${row.tanggal}).png`}`,
+                              "_blank"
+                            )
+                          }
+                          variant="success"
+                          icon={
+                            <Icon
+                              icon="game-icons:target-poster"
+                              width={15}
+                              height={15}
+                            />
                           }
                         />
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
-
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.tipe_kegiatan}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.nm_kegiatan}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.narsum}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.ruangan}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.tanggal}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {`${row.waktu} - ${row.waktu_end}`}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50">
+                        {row.status_ruangan === 1
+                          ? "ONLINE"
+                          : row.status_ruangan === 0
+                          ? "OFFLINE"
+                          : "HYBRID"}
+                      </td>
+                      <td className="text-sm border-2 border-white bg-gray-50 max-w-[8rem] truncate mx-auto">
+                        <div className="flex items-stretch gap-1">
+                          <Button.Icon
+                            as="a"
+                            href={`${prefix + menu.url}/invite/${row.id}`}
+                            variant="primary"
+                            icon={
+                              <Icon
+                                icon="bi:person-fill-add"
+                                width={15}
+                                height={15}
+                              />
+                            }
+                          />
+                          <ShowQr
+                            data={{
+                              token: row.token,
+                              kegiatan: row.nm_kegiatan,
+                            }}
+                          />
+                          <Button.Icon
+                            as="a"
+                            href={`${prefix + menu.url}/detail-list/${row.id}`}
+                            variant="primary"
+                            icon={
+                              <Icon
+                                icon="bxs:message-square-edit"
+                                width={15}
+                                height={15}
+                              />
+                            }
+                          />
+                          <Button.Icon
+                            variant="danger"
+                            icon={
+                              <Icon
+                                icon="solar:trash-bin-2-bold-duotone"
+                                width={15}
+                                height={15}
+                              />
+                            }
+                            onClick={() =>
+                              destroy(row.id).then(() => refreshAbsensi())
+                            }
+                          />
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
         <div className="flex mt-8">
           <div className="flex gap-1 ml-auto">
             <Button.Icon
