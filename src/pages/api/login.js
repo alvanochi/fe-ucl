779f00377ad714de5c7bac5b985905c1bdb55a41
Azisprtm_ <azisprtm2@gmail.com>
2026-03-08 13:16:33 +0700

    new update

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -3,14 +3,18 @@ import { withSessionRoute } from "../../lib/withSession";
 export default withSessionRoute(loginRoute);
 
 async function loginRoute(req, res) {
-  const userData = {
-    ...req.body,
-    is_logged_in: true,
-  };
+  const userData = { ...req.body };
+
+  if (userData.personalData?.embed_wajah) {
+    delete userData.personalData.embed_wajah;
+  }
+
+  userData.is_logged_in = true;
 
   req.session.user = userData;
 
   await req.session.save();
+
   res.status(200).send({
     status_code: 200,
     data: userData,
